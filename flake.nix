aefe460353e25f6fa802f3b86ab1e0d56f752b27
aceinetx <mouselin82@gmail.com>
2026-08-22 21:27:43 +0300

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "paperstrap";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        rustToolchain = fenix.packages.${system}.complete.toolchain;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "paperstrap";
+          version = "1.0.0";
+
+          src = self;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            cargo-expand
+          ];
+        };
+      }
+    );
+}
